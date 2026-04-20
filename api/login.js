725502113d1b62cd86eb1759586da03c87cc6b0b
BEAST-INSANE <139569983+BEAST-INSANE@users.noreply.github.com>
2026-04-20 20:35:13 +0530

    Create login.js

---
+++ api/login.js
@@ -0,0 +1,9 @@
+export default function handler(req, res) {
+  const { password } = req.body;
+
+  if (password === process.env.DASHBOARD_PASSWORD) {
+    return res.status(200).json({ success: true });
+  }
+
+  return res.status(401).json({ success: false });
+}
