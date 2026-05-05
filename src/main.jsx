1e59d87e33bc75f073f521750fb5d0ee41df05a1
BEAST-INSANE <139569983+BEAST-INSANE@users.noreply.github.com>
2026-05-06 01:52:55 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-
 import App from "./App.jsx";
 import Dashboard from "./Dashboard.jsx";
 
