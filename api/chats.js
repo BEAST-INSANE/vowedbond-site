d04b6f4c136e69ad9035cebfe26dfd57cbb438b5
BEAST-INSANE <139569983+BEAST-INSANE@users.noreply.github.com>
2026-05-08 00:47:18 +0530

    Update chats.js

--- api/chats.js
+++ api/chats.js
@@ -11,6 +11,7 @@ export default async function handler(req, res) {
     );
 
     const data = await response.json();
+
     return res.status(200).json(data);
   } catch (error) {
     return res.status(500).json({ error: error.message });
