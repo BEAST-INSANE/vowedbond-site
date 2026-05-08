24be2bfbd6f0419a3bd94f765490af18908dd186
BEAST-INSANE <139569983+BEAST-INSANE@users.noreply.github.com>
2026-05-08 23:31:43 +0530

    Update getReply.js

--- api/getReply.js
+++ api/getReply.js
@@ -1,9 +1,11 @@
 export default async function handler(req, res) {
+
   try {
+
     const { userName } = req.query;
 
     const response = await fetch(
-      `${process.env.SUPABASE_URL}/rest/v1/chats?user_name=eq.${userName}&select=admin_reply&order=created_at.desc&limit=1`,
+      `${process.env.SUPABASE_URL}/rest/v1/chats?user_name=eq.${userName}&admin_reply=not.is.null&select=admin_reply&order=created_at.desc&limit=1`,
       {
         headers: {
           apikey: process.env.SUPABASE_ANON_KEY,
@@ -15,7 +17,12 @@ export default async function handler(req, res) {
     const data = await response.json();
 
     return res.status(200).json(data[0] || {});
+
   } catch (error) {
-    return res.status(500).json({ error: error.message });
+
+    return res.status(500).json({
+      error: error.message
+    });
+
   }
 }
