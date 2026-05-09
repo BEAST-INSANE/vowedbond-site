e47bcc29482a271690b86a94460d341bf5582292
BEAST-INSANE <139569983+BEAST-INSANE@users.noreply.github.com>
2026-05-09 21:26:02 +0530

    Update Dashboard.jsx

--- src/Dashboard.jsx
+++ src/Dashboard.jsx
@@ -100,11 +100,11 @@ export default function Dashboard() {
 
   // FULL CONVERSATION
   const conversation = rows.filter(
-.reverse()
     (msg) =>
       msg.user_name ===
       selectedChat?.user_name
-  );
+  )
+.reverse();
 
   // LOGIN SCREEN
   if (!authorized) {
