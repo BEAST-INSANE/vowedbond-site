d7e4ce1ac65f1a42d8f029740f7583af1dd275a7
BEAST-INSANE <139569983+BEAST-INSANE@users.noreply.github.com>
2026-05-20 23:22:26 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -372,19 +372,19 @@ export default function App() {
       </div>
 
       {/* CHAT BUTTON */}
-   <button
+  <button
   onClick={() =>
     setChatOpen(
       !chatOpen
     )
   }
-  className="fixed bottom-6 right-6 z-50"
+  className="fixed bottom-6 right-6 z-50 w-16 h-16 rounded-full bg-slate-900 border border-cyan-400 shadow-[0_0_25px_rgba(34,211,238,0.5)] flex items-center justify-center hover:scale-110 transition"
 >
 
   <img
     src="/chatbot-logo.png"
     alt="Chat"
-    className="w-24 h-24 scale-[2.2] object-contain"
+    className="w-10 h-10 scale-[1.7] object-contain"
   />
 
 </button>
