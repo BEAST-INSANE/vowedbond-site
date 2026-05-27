9dce16546777f4019de0595b5434e2b740d952f0
BEAST-INSANE <139569983+BEAST-INSANE@users.noreply.github.com>
2026-05-28 00:49:11 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -278,7 +278,9 @@ export default function App() {
 
         const data =
           await res.json();
-
+   await new Promise((resolve) =>
+    setTimeout(resolve, 1200)
+   );
         setMessages((prev) => [
           ...prev,
           {
