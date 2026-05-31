9189e59fe863d1909fde688c07d7008e08901965
BEAST-INSANE <139569983+BEAST-INSANE@users.noreply.github.com>
2026-05-31 19:15:25 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -357,7 +357,7 @@ export default function App() {
             <img
   src="/logo.png"
   alt="Vowed Bond"
-  className="h-64 w-auto object-contain -mt-20 -mb-34"
+  className="h-64 w-auto object-contain -mt-20 -mb-26"
 />
           </button>
 
