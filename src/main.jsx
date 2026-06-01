355e544921d3b461c1a500e6bc6a399614dae12d
BEAST-INSANE <139569983+BEAST-INSANE@users.noreply.github.com>
2026-06-01 17:38:06 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import { HashRouter, Routes, Route } from "react-router-dom";
 import App from "./App.jsx";
-import Dashboard from "./Dashboard.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </HashRouter>
-  </React.StrictMode>
+  <App />
 );
