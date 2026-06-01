09b0616ea8ccd3b0660720fc6f20e21baa31df69
BEAST-INSANE <139569983+BEAST-INSANE@users.noreply.github.com>
2026-06-01 17:20:52 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,17 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import App from "./App.jsx";
-import Dashboard from "./Dashboard.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path="/dashboard" element={<Dashboard />} />
-      </Routes>
-    </BrowserRouter>
-  </React.StrictMode>
+  <h1 style={{ color: "red", fontSize: "50px" }}>
+    MAIN JSX TEST
+  </h1>
 );
