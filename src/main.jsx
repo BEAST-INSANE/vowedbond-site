d6d42d37fb0dbec5c903c24434776c7f6481ad34
BEAST-INSANE <139569983+BEAST-INSANE@users.noreply.github.com>
2026-06-02 15:02:57 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,8 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import App from "./App.jsx";
 import Dashboard from "./Dashboard.jsx";
+import RestaurantDemo from "./demo/RestaurantDemo.jsx";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
@@ -11,6 +13,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/restaurant-demo" element={<RestaurantDemo />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
