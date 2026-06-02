f3afdf9eab425224dbe701e75fa21f518767ec5d
BEAST-INSANE <139569983+BEAST-INSANE@users.noreply.github.com>
2026-06-02 15:44:34 +0530

    Update RestaurantDemo.jsx

--- src/demo/RestaurantDemo.jsx
+++ src/demo/RestaurantDemo.jsx
@@ -1,14 +1,7 @@
+import React from "react";
+
 export default function RestaurantDemo() {
   return (
-    <div
-      style={{
-        background: "white",
-        minHeight: "100vh",
-        padding: "40px",
-      }}
-    >
-      <h1>Bell Italia</h1>
-      <p>Authentic Italian Cuisine</p>
-    </div>
+    <h1>HELLO</h1>
   );
 }
