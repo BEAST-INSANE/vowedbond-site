0142037a9168954f4dcf307effd4667884f8e874
BEAST-INSANE <139569983+BEAST-INSANE@users.noreply.github.com>
2026-06-02 15:50:28 +0530

    Update RestaurantDemo.jsx

--- src/demo/RestaurantDemo.jsx
+++ src/demo/RestaurantDemo.jsx
@@ -10,7 +10,16 @@ export default function RestaurantDemo() {
       }}
     >
       <h1>Bell Italia</h1>
-      <p>Authentic Italian Cuisine</p>
+
+      <img
+        src="https://images.unsplash.com/photo-1513104890138-7c749659a591"
+        alt="Pizza"
+        style={{
+          width: "100%",
+          maxWidth: "500px",
+          borderRadius: "12px",
+        }}
+      />
     </div>
   );
 }
